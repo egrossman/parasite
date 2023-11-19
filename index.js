2726f3b4c7bb15cf86926fe135dbed213e852e2d
Grossman, Elan <egrossman3@gatech.edu>
2023-11-18 19:39:49 -0500

    make it better  2

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
       res.send('Hello from our server!')
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
       console.log('server listening on port 8080')
 })
