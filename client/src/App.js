1fd11844a2dc3bdf723a35b5feff8b75622dc63a
xIndMan7 <manava3@illinois.edu>
2023-11-19 03:50:46 -0500

    Initial chat support, package and hardcoded messages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import React from "react";
+import ChatBot from "react-simple-chatbot";
+import { Segment } from "semantic-ui-react";
 
 
 const apiCall = () => {
@@ -12,15 +15,38 @@ const apiCall = () => {
 
 
 function App() {
+  const steps = [{
+    id: "greet",
+    message: "Welcome!",
+    trigger: "waiting"
+  },
+  {
+    id: "waiting",
+    user: true,
+    trigger: "next"
+  },
+  {
+    id: "next",
+    message: "we will solve it right away!",
+    trigger: "waiting"
+  }]
+
   return (
     <div className="App">
       <header className="App-header">
 
         <div>
-        <button onClick={apiCall}>Make API Call</button>
+          <button onClick={apiCall}>Make API Call</button>
         </div>
+        <>
+          <Segment floated="right">
+            <ChatBot steps={steps}></ChatBot>
+          </Segment>
+        </>
       </header>
+
     </div>
+
   );
 }
 
